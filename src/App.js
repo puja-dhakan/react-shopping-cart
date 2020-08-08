9cbc849a58ac0a738cb3c231eb93f3767589bfc8
ShyamYadav16 <ShyamYadav16@syadav@ominto.com>
2020-08-08 16:27:02 +0530

    Add images and product component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import data from "./data.json";
+import Products from "./components/Products"
 
 
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:","
+    };
+  }
+
+  render(){
+    
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products}>
+
+          </Products>
+
+            
+
+            </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>
         All Rights Reserved.
@@ -17,6 +40,7 @@ function App() {
       
     </div>
   );
+  }
 }
 
 export default App;
