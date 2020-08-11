26bbfffbf1933cb875a841c9e6adc38945dfbbd8
ShyamYadav16 <ShyamYadav16@syadav@ominto.com>
2020-08-11 20:52:21 +0530

    cart component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from './components/Filter';
+import Cart from './components/Cart';
+
 
 
 
@@ -11,10 +13,32 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size:"",
       sort:","
     };
   }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+    cartItems: cartItems.filter((x)=>x._id !== product._id),
+  }); 
+};
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if(item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({cartItems});
+  }
+
   sortProducts = (event) => {
     const sort = event.target.value;
     console.log(event.target.value);
@@ -53,7 +77,7 @@ class App extends React.Component {
   
 
 };
-  render(){
+  render() {
     
   return (
     <div className="grid-container">
@@ -70,15 +94,13 @@ class App extends React.Component {
               sortProducts={this.sortProducts}>
 
             </Filter>
-            <Products products={this.state.products}>
-
-          </Products>
-
-            
+            <Products products={this.state.products} addToCart={this.addToCart}></Products>  
 
             </div>
-          <div className="sidebar">Cart Items</div>
-        </div>
+          <div className="sidebar"><Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}></Cart>
+          </div>
+          </div>
+        
       </main>
       <footer>
         All Rights Reserved.
@@ -88,5 +110,7 @@ class App extends React.Component {
   );
   }
 }
+
+
 
 export default App;
