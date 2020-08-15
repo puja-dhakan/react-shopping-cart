88778dfbe0a5e4de5d9ff3775734ba44f0d6c905
ShyamYadav16 <ShyamYadav16@syadav@ominto.com>
2020-08-15 20:13:32 +0530

    animation-modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react'
-import formatCurrency from '../util'
+import React, { Component } from 'react';
+import formatCurrency from '../util';
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 
-export default class  extends Component {
+export default class extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            product: null,
+        };
+    }
+    openModal = (product) => {
+        this.setState({product});
+    };
+    closeModel = () => {
+        this.setState({product : null});
+    };
     render() {
+        const {product} = this.state;
         return (
             <div>
+                <Fade bottom cascade>
                 <ul className="products">
                     {this.props.products.map((product) => (
                         <li key={product._id}>
                             <div className="product">
-                                <a href={"#" + product._id}>
+                                <a href={"#" + product._id} onClick={() => this.openModal(product)}>
                                     <img src={product.image} alt="product.title"></img>
                                     <p>
                                         {product.title}
@@ -31,6 +48,51 @@ export default class  extends Component {
                         </li>
                     ))}
                 </ul>
+                </Fade>
+                {product && (
+                        <Modal isOpen={true}
+                        onRequestClose={this.closeModel}>
+                            <Zoom>
+                                <button className="close-modal" onClick={this.closeModel}>
+                                    X
+                                </button>
+                                <div className="product-details">
+                                    <img src={product.image} alt={product.title}></img>
+                                    <div className="product-details-description">
+                                        <p>
+                                            <strong>{product.title}</strong>
+                                        </p>
+                                        <p>
+                                            {product.description}
+                                        </p>
+                                        <p>
+                                            Available Size
+                                            {product.availableSizes.map(x => (
+                                                <span> {" "}
+                                                    <button className="button">{x}</button>
+                                                    </span>
+                                            ))}
+                                        </p>
+                                        <div className="product-price">
+                                            <div>{formatCurrency (product.price)} </div>
+                                            <button
+                                            className="button primary"
+                                            onClick={() => {
+                                            this.props.addToCart(product);
+                                            this.closeModel();
+                                            }}>
+                                                Add To Cart
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    Modal
+                                </div>
+                            </Zoom>
+                        </Modal>
+                    )
+                }
             </div>
         );
     }
